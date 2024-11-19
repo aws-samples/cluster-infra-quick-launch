9419ac160d33fbd02c2b0715719b5d572db30d2d
haozhx23 <121946073+haozhx23@users.noreply.github.com>
2024-11-20 01:16:40 +0800

    Update 3_ssh_to_cluster.sh

--- SMHP/3_ssh_to_cluster.sh
+++ SMHP/3_ssh_to_cluster.sh
@@ -5,5 +5,9 @@
 cluster_name=$(jq -r .ClusterName cluster-config.json)
 login_group_name=$(jq -r '.InstanceGroups[0].InstanceGroupName' cluster-config.json)
 
-chmod +x awsome-distributed-training/1.architectures/5.sagemaker-hyperpod/easy-ssh.sh
-./awsome-distributed-training/1.architectures/5.sagemaker-hyperpod/easy-ssh.sh -c $login_group_name $cluster_name
+# chmod +x awsome-distributed-training/1.architectures/5.sagemaker-hyperpod/easy-ssh.sh
+# ./awsome-distributed-training/1.architectures/5.sagemaker-hyperpod/easy-ssh.sh -c $login_group_name $cluster_name
+
+curl -O https://raw.githubusercontent.com/aws-samples/awsome-distributed-training/main/1.architectures/5.sagemaker-hyperpod/easy-ssh.sh
+chmod +x easy-ssh.sh
+./easy-ssh.sh -c $controller_group_name $cluster_name
