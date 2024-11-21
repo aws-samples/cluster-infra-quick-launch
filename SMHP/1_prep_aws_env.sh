276f84d7c5a6f988cfa69686ea6f7c27df84d617
haozhx23 <121946073+haozhx23@users.noreply.github.com>
2024-11-21 16:40:41 +0800

    Update 1_prep_aws_env.sh

--- SMHP/1_prep_aws_env.sh
+++ SMHP/1_prep_aws_env.sh
@@ -1,10 +1,12 @@
-aws configure
-
-pip3 install -U boto3 omegaconf
+sudo apt update
+sudo apt install jq -y
+sudo apt install unzip
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --update
+
+aws configure
 
 # https://docs.aws.amazon.com/systems-manager/latest/userguide/install-plugin-debian-and-ubuntu.html
 echo "--Install SSM on Ubuntu--"
@@ -20,9 +22,8 @@ echo `aws sagemaker help | grep cluster`
 
 git clone --depth=1 https://github.com/aws-samples/awsome-distributed-training/
 
-# c9 ubuntu env
-# sudo apt update
-# sudo apt install jq -y
+pip install -U pip
+pip install -U boto3 omegaconf
 
 # change lifecycle config if need
 # awsome-distributed-training/1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/config.py
